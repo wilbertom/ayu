31643388c13bcb79735d2f9ccea9df70c7327378
Wilberto Morales <wilbertomorales777@gmail.com>
2014-11-19 01:11:49 -0600

    Add requests to requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author = "Wilberto Morales",
     author_email = "wilbertomorales777@gmail.com",
     description = ("Process to keep an eye on website health."),
+    install_requires=['requests==2.4.3'],
     license = "",
     url = "https://github.com/wilbertom/ayu",
     packages=['ayu'],
