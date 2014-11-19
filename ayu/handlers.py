5b88a4fa3d012d8582ecbddfef4b9cd8ba5fd27b
Wilberto Morales <wilbertomorales777@gmail.com>
2014-11-19 02:13:54 -0600

    Email and time delayed handlers

--- ayu/handlers.py
+++ ayu/handlers.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
 import csv
 import logging
+from datetime import datetime
+
 
 class BaseHandler(metaclass=ABCMeta):
 
@@ -13,7 +15,7 @@ class BaseHandler(metaclass=ABCMeta):
         pass
 
 
-class StdoutHandler(BaseHandler):
+class StdOutHandler(BaseHandler):
 
     def on_up(self, result):
         print('{} is ok...'.format(result.uri))
@@ -53,3 +55,77 @@ class LogHandler(BaseHandler):
 
     def on_down(self, result):
         return self.logger.warning('{} was down at {}'.format(result.uri, result.time))
+
+
+class DeltaHandler(BaseHandler):
+
+    def __init__(self, time_delta, logger=None):
+        self.d = time_delta
+        self._alert_times = {}
+        self.logger = logger
+
+    def _should_alert_error_for(self, uri):
+        last_alert = self._alert_times.get(uri)
+        return last_alert is None or (datetime.now() - last_alert) >= self.d
+
+    def alert(self, result):
+        self._alert_times[result.uri] = datetime.now()
+        return self
+
+    def on_down(self, result):
+        """
+        When a service is down the subscriber will be alerted.
+        We only alert once every d delta so that the subscriber
+        will not be annoyed and has time to respond.
+
+        """
+
+        if self.logger is not None:
+            self.logger.warn('{} was down'.format(result.uri))
+
+        if self._should_alert_error_for(result.uri):
+            self.alert(result)
+
+        return self
+
+    def on_up(self, result):
+        raise NotImplementedError('This handler only takes care of down alerts.')
+
+
+class EmailHandler(DeltaHandler):
+
+    def __init__(self, email_address, time_delta, subscriber, logger=None):
+        super().__init__(time_delta, logger=logger)
+        self.email_address = email_address
+        self.c = subscriber
+
+    def format_subject(self, result):
+        raise NotImplementedError('Need to format the subject')
+
+    def format_body(self, result):
+        raise NotImplementedError('Need to format the body')
+
+    def alert(self, result):
+        super().alert(result)
+
+        if self.logger is not None:
+            self.logger.info('Sending email alert.')
+
+        # generate what will be sent to the subscriber
+        subject = self.format_subject(result)
+        content = self.format_body(result)
+
+        # send the email alerting that the website is down
+        return self.c.send(self.email_address, subject, content)
+
+    def on_up(self, result):
+        """
+        We don't care when the website is up so we don't do anything.
+        """
+        if self.logger is not None:
+            self.logger.info('{} is up'.format(result.uri))
+
+        return self
+
+
+
